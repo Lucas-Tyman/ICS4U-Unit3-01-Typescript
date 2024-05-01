989362f43f228e9f4750850c40beced92c54e840
Lucas Tyman <lucas.tyman@mths.ca>
2024-05-01 13:15:51 +0000

    index

--- index.ts
+++ index.ts
@@ -1,37 +1,27 @@
-""" this program reverses a string using recursion """
-#!/usr/bin/env python3
+/** This program reverses a string using recursion
+*
+* By: Lucas Tyman
+* Version: 1.0
+* Since: 2024-04-29
+*/
 
-# Created by: Mr. Coxall
-# Created on: Sep 2020
-def reverse_string(string_to_reverse: str) -› str:
-    """ this function reverses a string, using recursion
-        @param accepts a string
-        @return the string backwards, using recursion
-    """
+import { createPrompt } from 'bun-promptx'
 
-    if string_to_reverse == "":
-        return ""
-    else:
-        # in python a string is a list of characters
-        # [1:] returns the 1st (not the oth!) character to the end
-        # 0] returns the 0th character
-        first_character_of_string = string_to_reverse[®]
-        rest_of__string = string_to_reverse[1:]
+// Reverses strings using recursion.
+function reverseString(stringToReverse: string): string {
+  if (stringToReverse == "") {
+    return ""
+  } else {
+    let firstCharacter: string = stringToReverse[0]
+    let restOfString: string = stringToReverse.slice(1, stringToReverse.length)
+  
+    return reverseString(restOfString) + firstCharacter
+  }
 
-        # NOTE: the "reversed" order of the following line
-        return reverse_string (rest_of_string) + first_character_of_string
+// ASk for user input
+const selectedString = createPrompt('Enter a string to reverse: ').value
 
-
-def main() -> None:
-    """ this main function calls a recursive function """
-    a_string = "recussion"
-
-    print("")
-    print(f"The original string is: {a_string}")
-    the_reversed_string = reverse_string(a_string)
-    print(f"The reversed string is: {the_reversed_string)")
-
-    print ("\nDone.")
-
-if __name == "__main__":
-    main()
+// Shows original word
+console.log(`The reversed string is ${reverseString(selectedString)}`);
+                       
+console.log('\nDone.')
